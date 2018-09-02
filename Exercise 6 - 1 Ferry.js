0dbd49572343329658f82e285d6151629ca81b38
feihidajat <ferrymuhammadh@gmail.com>
2018-09-02 21:45:09 +0700

    exercise 6
    tugas

---
+++ Exercise 6 - 1 Ferry.js
@@ -0,0 +1,13 @@
+var flag = 2;
+var flag2 = 20;
+console.log('Looping Pertama')
+while(flag <= 20) {
+  console.log(flag + '- I love coding');
+  flag = flag + 2;
+}
+flag = 2;
+console.log('Looping Kedua')
+while(flag <= 20 && flag2 >=2) {
+  console.log(flag2 + '- I will be a fullstack dev');
+  flag2 = flag2 - 2;
+}
---
+++ Exercise 6 - 2 Ferry.js
@@ -0,0 +1,12 @@
+console.log('Looping Pertama');
+for(flag=2; flag<=20; flag++) {
+  console.log(flag + '- I love coding' );
+  flag = flag + 1;
+}
+
+flag = 2;
+console.log('Looping Kedua')
+for(flag=20;flag>=2; flag-- ) {
+  console.log(flag + '- I will be a fullstack dev');
+  flag = flag - 1;
+}
